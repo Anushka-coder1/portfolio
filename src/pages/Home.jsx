6e9e23fd83418cdce068da30079842993f81b6f5
Anushka-coder1 <127anushka@gmail.com>
2026-01-23 20:50:01 +0530

    successfully created home and about sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import  {StarBackground } from "../components/StarBackground"
+import { Navbar } from "../components/NavBar"
+import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
+
+
+const Home = () => {
+  return (
+    <div className="bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle/>
+
+      {/* Background effects */}
+      <StarBackground/>
+
+      {/* Navbar */}
+      <Navbar/>
+  
+      {/* Main Content */}
+      <main>
+        <HeroSection/>
+        <AboutSection/>
+      </main>
+
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
