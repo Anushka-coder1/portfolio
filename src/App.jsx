a56fd2c2c2d9651c7b51586e166c67223b86b31e
Anushka-coder1 <127anushka@gmail.com>
2026-01-24 17:32:32 +0530

    added all the other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import  {StarBackground } from "../components/StarBackground"
 import { Navbar } from "../components/NavBar"
 import { HeroSection } from "../components/HeroSection"
 import { AboutSection } from "../components/AboutSection"
-
+import { SkillsSection } from "../components/SkillsSection"
+import { ProjectsSection } from "../components/ProjectsSection"
+import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
 
 const Home = () => {
   return (
@@ -21,9 +24,13 @@ const Home = () => {
       <main>
         <HeroSection/>
         <AboutSection/>
+        <SkillsSection/>
+        <ProjectsSection/>
+        <ContactSection/>
       </main>
 
       {/* Footer */}
+      <Footer/>
     </div>
   )
 }
