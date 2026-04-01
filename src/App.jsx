de2aadc5225aa5a198fd60c6bf661e8c55ea3403
Anushka-coder1 <127anushka@gmail.com>
2026-04-01 14:52:02 +0530

    fixed not found page issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
           <Route path="/NotFound" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
