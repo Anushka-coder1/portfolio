b9638a158c89890a3d44b0cf0c2b3e3f12289dbe
Anushka-coder1 <127anushka@gmail.com>
2026-04-01 22:52:45 +0530

    fixed menu icon 2

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -74,7 +74,7 @@ export const ThemeToggle = () => {
         )}
       >
         {isDarkMode ? (
-          <Sun className="h-6 w-6 text-yellow-300" />
+          <Sun className="h-6 w-6 text-white" />
         ) : (
           <Moon className="h-6 w-6 text-blue-900" />
         )}
